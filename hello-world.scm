e7330603c38bcb480a331b75108db02c74b7e14f
Yawar Amin <yawar.amin@gmail.com>
2011-07-09 17:34:44 -0400

    Add documentation

--- hello-world.scm
+++ hello-world.scm
@@ -1,3 +1,4 @@
+; Declarative reports for GnuCash
 (define-module (gnucash report hello-world))
 (use-modules (gnucash main))
 (use-modules (gnucash gnc-module))
@@ -6,8 +7,28 @@
 (gnc:module-load "gnucash/report/report-system" 0)
 (gnc:module-load "gnucash/html" 0)
 
+; d:report - Create a declarative report
+; Arguments:
+;  name - The name of the report. I don't really have a use for it yet, but
+;  am just trying to get names for all declared objects for future use.
+;
+;  defs - A list of definitions (defs). A def is like an object that is
+;  accessible throughout the body of the report, and also doubles as
+;  options in the report's options dialog box.
+;
+;  title - The title of the report. This should appear in the Reports menu
+;  and on the top of the report page.
+;
+;  subtitle - The report subtitle. This should appear right under the
+;  report title.
+;
+;  reportbuilders - A list of actions (called reportbuilders) which carry
+;  out the tasks needed to actually gather the data and generate the HTML
+;  on the report page.
+; Returns:
+;  The generated report.
 (define d:report
-  (lambda (name defs-list title subtitle reportbuilders)
+  (lambda (name defs title subtitle reportbuilders)
     (let ((options-generator
            (lambda ()
              (let ((options (gnc:new-options)))
@@ -34,9 +55,20 @@
        'options-generator options-generator
        'renderer renderer))))
 
+; d:filter-none - A top-level filter which doesn't filter any of the data
+; Arguments:
+;  reportbuilders - A list of reportbuilders.
+; Returns:
+;  reportbuilders.
 (define d:filter-none
   (lambda reportbuilders reportbuilders))
 
+; d:p - Generate a paragraph reportbuilder
+; Arguments:
+;  text - The text to put in the paragraph.
+; Returns:
+;  A function that, when called with the documents and options objects,
+;  adds an HTML paragraph to the report page.
 (define d:p
   (lambda (text)
     (lambda (document options)
