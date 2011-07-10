0d029e9f87b48cc753283d30301713aff3f787d6
Yawar Amin <yawar.amin@gmail.com>
2011-07-10 19:45:11 -0400

    Define date options
    ... and get them to show up in the report options dialog box. The next step
    is to get their values with a (date ...) function.

--- hello-world.scm
+++ hello-world.scm
@@ -1,4 +1,25 @@
 ; Declarative reports for GnuCash
+
+; Some functionality copied from:
+;
+; * The LAML library and programs written by Kurt Normark, Aalborg
+; University, Denmark.
+; Copyright (C) 1999-2009  Kurt Normark, normark@cs.aau.dk.
+;
+; This program is free software; you can redistribute it and/or modify
+; it under the terms of the GNU General Public License as published by
+; the Free Software Foundation; either version 2 of the License, or
+; (at your option) any later version.
+;
+; This program is distributed in the hope that it will be useful,
+; but WITHOUT ANY WARRANTY; without even the implied warranty of
+; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+; GNU General Public License for more details.
+;
+; You should have received a copy of the GNU General Public License
+; along with this program; if not, write to the Free Software
+; Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
 (define-module (gnucash report hello-world))
 (use-modules (gnucash main))
 (use-modules (gnucash gnc-module))
@@ -6,6 +27,38 @@
 (debug-enable 'backtrace)
 (gnc:module-load "gnucash/report/report-system" 0)
 (gnc:module-load "gnucash/html" 0)
+
+; take - Take some items from a list
+; Arguments:
+;  n - The number of items to take.
+;
+;  lst - The list to take from.
+; Returns:
+;  A list of the first n items from lst. If n is 0 or lst is an empty list,
+;  returns an empty list.
+(define (take n lst)
+  (cond ((= n 0) '())
+        ((null? lst) '())
+        (#t (cons (car lst) (take (- n 1) (cdr lst))))))
+
+; split-point - See
+; http://www.cs.aau.dk/~normark/scheme/lib/man/general.html#split-point
+(define (split-point ch str)
+ (call-with-current-continuation
+  (lambda (exit)
+   (cond ((equal? str "") #f)
+         ((eqv? ch (string-ref str 0)) 0)
+         (else (let ((res (split-point ch (substring str 1 (string-length str)))))
+                  (if (not res)
+                      (exit #f)
+                      (+ 1 res))))))))
+
+; split-on - See
+; http://www.cs.aau.dk/~normark/scheme/lib/man/general.html#split-on
+(define (split-on ch str)
+ (let ((sp (split-point ch str)))
+   (list (substring str 0 sp)
+         (substring str (+ sp 1) (string-length str)))))
 
 ; d:report - Create a declarative report
 ; Arguments:
@@ -32,6 +85,9 @@
     (let ((options-generator
            (lambda ()
              (let ((options (gnc:new-options)))
+               (map
+                (lambda (def) (def options))
+                defs)
                options)))
           (renderer
            (lambda (options)
@@ -55,6 +111,60 @@
        'options-generator options-generator
        'renderer renderer))))
 
+; d:defs - Tells options defs where they will be placed on the report
+; options dialog box.
+; Arguments:
+;  defs - A list of defs like date defs, account defs, etc.
+; Returns:
+;  A list of definition function which now know where they will be placed
+;  in the options dialog box, but need to be called with the report options
+;  object as their argument (this will happen in the d:report function
+;  where they are fed in as the defs argument).
+(define d:defs
+  (lambda defs
+    (map
+     (lambda (def pos) (def pos))
+     defs
+     (take
+      (length defs)
+      '("ca" "cb" "cc" "cd" "ce" "cf" "cg" "ch" "ci" "cj" "ck" "cl" "cm"
+        "cn" "co" "cp" "cq" "cr" "cs" "ct" "cu" "cv" "cx" "cy" "cz" "da"
+        "db" "dc" "de" "df" "dg" "dh" "di" "dj" "dk" "dl" "dm" "dn" "do"
+        "dp" "dq" "dr" "ds" "dt" "du" "dv" "dw" "dx" "dy" "dz")))))
+
+; d:def-date - A date definition
+; Arguments:
+;  name - The name of this date def
+;
+;  title - The title of this date def in the form of "Pagename/Widgetname"
+;  where Pagename is the page (tab) in the options dialog box where the def
+;  will be placed in the form of an option, and Widgetname is the name the
+;  option will be given on the dialog box.
+;
+;  date-str - A date string in the form 2011-12-31.
+; Returns:
+;  A date definition function that takes a positioning character "a"-"z" as
+;  its argument.
+(define d:def-date
+  (lambda (name title date-str)
+    (lambda (pos)
+      (lambda (options)
+        (let* ((name-split (split-on #\/ title))
+               (pagename (N_ (car name-split)))
+               (widgetname (N_ (cadr name-split))))
+          (gnc:register-option
+           options
+           (gnc:make-date-option
+            pagename
+            widgetname
+            pos
+            widgetname
+            (lambda ()
+              (cons 'absolute
+                    (cons (car (mktime (car (strptime "%Y-%m-%d" date-str)))) 0)))
+            #f
+            'absolute #f)))))))
+
 ; d:filter-none - A top-level filter which doesn't filter any of the data
 ; Arguments:
 ;  reportbuilders - A list of reportbuilders.
@@ -76,12 +186,13 @@
        document
        (gnc:make-html-text
         (gnc:html-markup-p
-         (gnc:html-markup/format
-          (_ text))))))))
+          (_ text)))))))
 
 (d:report
  "income-statement" ; name
- 0 ; defs
+ (d:defs
+   (d:def-date "start-date" "General/Start date" "2011-01-01")
+   (d:def-date "end-date" "General/End date" "2011-07-31"))
  ; Have to keep this title while experimenting in the sample report that
  ; comes with GnuCash
  "Hello, World" ; title
